499217991b55aa17acd29f0baffe79246c24f7cb
Alfredo <offsound2020@gmail.com>
2024-05-23 16:21:33 -0400

    Update

--- src/components/GetProfileInfo.tsx
+++ src/components/GetProfileInfo.tsx
@@ -1,6 +1,27 @@
 "use client"
 
 function GetProfileInfo() {
+
+  // const fetchProfile = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:3000/auth/profile", {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error("Error getting the profile");
+  //     }
+  //     const data = await response.json();
+  //     console.log(data)
+      
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // fetchProfile()
   return ( 
   <h1>Profile</h1>
  );
