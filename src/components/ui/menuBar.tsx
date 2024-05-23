90537111e059214b8e6fb27c0c4b4afb8958c803
Alfredo <offsound2020@gmail.com>
2024-05-23 09:38:01 -0400

    Update

--- src/components/ui/menuBar.tsx
+++ src/components/ui/menuBar.tsx
@@ -6,10 +6,10 @@ function MenuBar() {
   return (
     
     <div className="menu-bar bg-white w-100 absolute bottom-0 left-0 right-0 flex justify-between items-center px-6 py-4">
-      <Link href="/profile">
+      <Link href="/dashboard">
         <Image
           className="bg-white"
-          src="/user-regular.svg"
+          src="/grid.svg"
           width={30}
           height={30}
           alt="user icon"
@@ -19,7 +19,37 @@ function MenuBar() {
       <Link href="/dashboard">
         <Image
           className="bg-white"
-          src="/house-solid.svg"
+          src="/Group8224.svg"
+          width={30}
+          height={30}
+          alt="user icon"
+        ></Image>
+      </Link>
+
+      <Link href="/orders">
+        <Image
+          className="bg-white"
+          src="/Group1322.svg"
+          width={60}
+          height={60}
+          alt="user icon"
+        ></Image>
+      </Link>
+
+      <Link href="/dashboard">
+        <Image
+          className="bg-white"
+          src="/Group8223.svg"
+          width={30}
+          height={30}
+          alt="user icon"
+        ></Image>
+      </Link>
+
+      <Link href="/profile">
+        <Image
+          className="bg-white"
+          src="/Group8222.svg"
           width={30}
           height={30}
           alt="user icon"
