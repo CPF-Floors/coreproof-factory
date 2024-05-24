ad89e703f0ed724ca0df6a28a1372cc920383f3f
Alfredo <offsound2020@gmail.com>
2024-05-24 11:05:13 -0400

    Update

--- src/app/profile/GetProfileInfo.tsx
+++ src/app/profile/GetProfileInfo.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 function GetProfileInfo({ token }: { token: React.ReactNode }) {
   const [newProfile, setNewProfile] = useState([]);
@@ -15,7 +17,7 @@ function GetProfileInfo({ token }: { token: React.ReactNode }) {
             Authorization: `Bearer ${token}`,
           },
         });
-  
+
         if (!response.ok) {
           throw new Error("Error getting the profile");
         }
@@ -26,17 +28,62 @@ function GetProfileInfo({ token }: { token: React.ReactNode }) {
       }
     };
     fetchProfile();
-  }, [token])
+  }, [token]);
   console.log(newProfile);
 
   return (
     <>
+      <div className="profile-header-container">
+        <div className="flex flex-row items-center">
+          <Link href="/dashboard">
+            <Image
+              className="m-5"
+              src="/Back.svg"
+              height="40"
+              width="40"
+              alt="Back"
+            ></Image>
+          </Link>
+          <h2 className="font-semibold my-10 ">Personal Profile</h2>
+        </div>
 
-      <h2>Welcome, {newProfile.fullName}</h2>
-      <p>Address: {newProfile.address}</p>
-      <p>Company: {newProfile.businessName}</p>
-      <p>Your email: {newProfile.email}</p>
+        <div className="px-10">
+          <h1 className="font-semibold text-lg">
+            Welcome, {newProfile.fullName}
+          </h1>
+          <p>{newProfile.businessName}</p>
+          <p>{newProfile.role}</p>
+        </div>
+      </div>
 
+      <div className="p-5 bg-gray-100 m-5 rounded-2xl flex">
+        <Image
+          src="./profileUser.svg"
+          width={60}
+          height={60}
+          alt="user"
+        ></Image>
+        <div className="m-5">
+          <h2 className="font-semibold">FULL NAME</h2>
+          <p>{newProfile.fullName}</p>
+        </div>
+      </div>
+
+      <div className="p-5 bg-gray-100 m-5 rounded-2xl flex">
+        <Image src="./mail.svg" width={60} height={60} alt="user"></Image>
+        <div className="m-5">
+          <h2 className="font-semibold">EMAIL</h2>
+          <p>{newProfile.email}</p>
+        </div>
+      </div>
+
+      <div className="p-5 bg-gray-100 m-5 rounded-2xl flex">
+        <Image src="./phone.svg" width={60} height={60} alt="user"></Image>
+        <div className="m-5">
+          <h2 className="font-semibold">PHONE NUMBER</h2>
+          <p>{newProfile.phoneNumber}</p>
+        </div>
+      </div>
     </>
   );
 }
