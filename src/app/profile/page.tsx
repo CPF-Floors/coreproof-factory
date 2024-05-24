29b7f3ef45172ab0840f94b7be96d2b22eec49e7
Alfredo <offsound2020@gmail.com>
2024-05-24 07:51:10 -0400

    Update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,12 @@
 import { cookies } from "next/headers";
 import GetProfileInfo from "./GetProfileInfo";
 
+interface CookieStore {
+  // Define properties and their types here
+  // Example:
+  get(key: string): { value?: string; } | null;
+}
+
 function Profile() {
   const cookieStore = cookies();
   const token = cookieStore.get("token")?.value;
