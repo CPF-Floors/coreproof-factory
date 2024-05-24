8742b705e43e6eb2e1c7c89e00d57546b964d65e
Alfredo <offsound2020@gmail.com>
2024-05-24 10:35:56 -0400

    Update

--- src/app/profile/GetProfileInfo.tsx
+++ src/app/profile/GetProfileInfo.tsx
@@ -1,44 +1,42 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+function GetProfileInfo({ token }: { token: React.ReactNode }) {
+  const [newProfile, setNewProfile] = useState([]);
 
-function GetProfileInfo({ token } : {token: React.ReactNode}) {
-
-  const [newProfile, setNewProfile] = useState([])
-
-
+  useEffect(() => {
     const fetchProfile = async () => {
       try {
         const response = await fetch("http://localhost:3000/auth/profile", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
+            Authorization: `Bearer ${token}`,
           },
         });
   
         if (!response.ok) {
           throw new Error("Error getting the profile");
         }
-  
         const data = await response.json();
-        setNewProfile(data)
-  
-  
+        setNewProfile(data);
       } catch (error) {
         console.error(error);
       }
     };
-    fetchProfile()
-    console.log(newProfile)
-
-
+    fetchProfile();
+  }, [token])
+  console.log(newProfile);
 
   return (
     <>
-        <h1>{token ? "Profile" : "Loading..."}</h1>
-        <h2>Welcome, {newProfile.fullName}</h2>
+
+      <h2>Welcome, {newProfile.fullName}</h2>
+      <p>Address: {newProfile.address}</p>
+      <p>Company: {newProfile.businessName}</p>
+      <p>Your email: {newProfile.email}</p>
+
     </>
   );
 }
